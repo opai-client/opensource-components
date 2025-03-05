ea5e0848a1212176f15cc97f5e3dadfbd7bf1be7
cubk <cubk@vip.qq.com>
2025-03-05 22:07:40 +0800

    feat: more features

--- src/main/java/today/opai/api/interfaces/game/world/World.java
+++ src/main/java/today/opai/api/interfaces/game/world/World.java
@@ -1,6 +1,7 @@
 package today.opai.api.interfaces.game.world;
 
 import today.opai.api.dataset.BlockPosition;
+import today.opai.api.dataset.BoundingBox;
 import today.opai.api.interfaces.game.entity.Entity;
 import today.opai.api.interfaces.game.entity.Player;
 
@@ -16,6 +17,8 @@ public interface World {
     void removeEntity(Entity entity);
 
     Block getBlockFromPosition(BlockPosition position);
+
+    BoundingBox getBoundingBox(BlockPosition position);
 
     @Deprecated
     int getBlock(BlockPosition position);
