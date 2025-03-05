c8445b277338b35c558a5d800fdce09ba7f761a2
cubk <cubk@vip.qq.com>
2025-03-05 22:23:27 +0800

    fix: access issue

--- src/main/java/today/opai/api/features/ExtensionScreen.java
+++ src/main/java/today/opai/api/features/ExtensionScreen.java
@@ -1,13 +1,13 @@
 package today.opai.api.features;
 
 public abstract class ExtensionScreen {
-    protected void keyTyped(char chr, int key) {
+    public void keyTyped(char chr, int key) {
     }
 
-    protected void mouseClicked(int mouseX, int mouseY, int button) {
+    public void mouseClicked(int mouseX, int mouseY, int button) {
     }
 
-    protected void mouseMovedOrUp(int mouseX, int mouseY, int button) {
+    public void mouseMovedOrUp(int mouseX, int mouseY, int button) {
     }
 
     public void drawScreen(int mouseX, int mouseY) {
