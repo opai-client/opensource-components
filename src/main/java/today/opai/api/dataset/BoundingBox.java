404952e4447ece5b873c4fd1e1139204238baa81
cubk <cubk@vip.qq.com>
2025-03-05 22:10:22 +0800

    style: use lombok

--- src/main/java/today/opai/api/dataset/BoundingBox.java
+++ src/main/java/today/opai/api/dataset/BoundingBox.java
@@ -1,19 +1,11 @@
 package today.opai.api.dataset;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class BoundingBox {
 	Vec3Data min;
 	Vec3Data max;
-
-	public BoundingBox(Vec3Data min, Vec3Data max) {
-		this.min = min;
-		this.max = max;
-	}
-
-	public Vec3Data getMin() {
-		return this.min;
-	}
-
-	public Vec3Data getMax() {
-		return this.max;
-	}
 }
